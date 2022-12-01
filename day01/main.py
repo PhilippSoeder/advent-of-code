3761c7aedd2ecf9c2defaa33d3efb88e7c775e0f
Philipp Söder <philippsoeder@gmx.de>
2022-12-01 06:53:12 +0100

    Optimise day01

--- day01/main.py
+++ day01/main.py
@@ -3,9 +3,10 @@ Advent of Code 2022 - Day 1
 '''
 
 
-def get_list_of_calories():
+def get_list_of_calories() -> list:
     with open("input.txt") as input:
         lines = input.read().splitlines()
+    lines.append('')
     tmp_count = 0
     calories_list = []
     for line in lines:
@@ -17,21 +18,16 @@ def get_list_of_calories():
     return calories_list
 
 
-def part1(calories_list):
+def part1(calories_list: list) -> int:
     return max(calories_list)
 
 
-def part2(calories_list):
+def part2(calories_list: list) -> int:
     calories_list.sort(reverse=True)
-    calories_list = calories_list[:3]
-    return sum(calories_list)
+    return sum(calories_list[:3])
 
 
 if __name__ == '__main__':
     calories_list = get_list_of_calories()
-
-    # Part 1
     print(f'Part 1: {part1(calories_list)}')
-
-    # Part 2
     print(f'Part 2: {part2(calories_list)}')
