baab1434adf267073397e82a2dc8adf76b67ae41
Philipp Söder <philippsoeder@gmx.de>
2022-12-05 07:30:51 +0100

    Day 5

---
+++ day05/main.py
@@ -0,0 +1,103 @@
+'''
+Advent of Code 2022 - Day 5
+'''
+import aocd
+import re
+
+
+def part1() -> int:
+    for instruction in instructions:
+        amount, from_index, to_index = re.findall(r'[0-9]+', instruction)
+        for _ in range(int(amount)):
+            c = crates[int(from_index)-1].pop()
+            crates[int(to_index)-1].append(c)
+    result = []
+    for stack in crates:
+        p = stack.pop()
+        result.append(p)
+    result = "".join([str(i) for i in result])
+    return result
+
+
+def part2() -> int:
+    for instruction in instructions:
+        amount, from_index, to_index = re.findall(r'[0-9]+', instruction)
+        tmp = []
+        for _ in range(int(amount)):
+            c = crates[int(from_index)-1].pop()
+            tmp.append(c)
+        crates[int(to_index)-1].extend(list(reversed(tmp)))
+    result = []
+    for stack in crates:
+        p = stack.pop()
+        result.append(p)
+    result = "".join([str(i) for i in result])
+    return result
+
+
+def get_number_of_crates():
+    for i, line in enumerate(input):
+        for char in line:
+            if char == ['[']:
+                break
+            elif char == '1':
+                print(line)
+                for c in line:
+                    if c != ' ':
+                        number_crates = c
+                return [int(number_crates), i]
+
+
+def get_stacks_of_crates(n, m):
+    stack = {}
+    for l, line in enumerate(input):
+        if l < m:
+            for i in range(0, n*3+n-1, 4):
+                if line[i] == '[':
+                    print(line[i])
+                    print(i)
+    return 0
+
+
+if __name__ == '__main__':
+    AOC_YEAR = 2022
+    AOC_DAY = 5
+    input = open("input.txt").read().splitlines()
+
+    crates = [
+        ['H', 'B', 'V', 'W', 'N', 'M', 'L', 'P'],
+        ['M', 'Q', 'H'],
+        ['N', 'D', 'B', 'G', 'F', 'Q', 'M', 'L'],
+        ['Z', 'T', 'F', 'Q', 'M', 'W', 'G'],
+        ['M', 'T', 'H', 'P'],
+        ['C', 'B', 'M', 'J', 'D', 'H', 'G', 'T'],
+        ['M', 'N', 'B', 'F', 'V', 'R'],
+        ['P', 'L', 'H', 'M', 'R', 'G', 'S'],
+        ['P', 'D', 'B', 'C', 'N']
+    ]
+
+    instructions = []
+    for i, line in enumerate(input):
+        if i > 9:
+            instructions.append(line)
+
+    a = part1()
+    print(f'{a = }')
+
+    crates = [
+        ['H', 'B', 'V', 'W', 'N', 'M', 'L', 'P'],
+        ['M', 'Q', 'H'],
+        ['N', 'D', 'B', 'G', 'F', 'Q', 'M', 'L'],
+        ['Z', 'T', 'F', 'Q', 'M', 'W', 'G'],
+        ['M', 'T', 'H', 'P'],
+        ['C', 'B', 'M', 'J', 'D', 'H', 'G', 'T'],
+        ['M', 'N', 'B', 'F', 'V', 'R'],
+        ['P', 'L', 'H', 'M', 'R', 'G', 'S'],
+        ['P', 'D', 'B', 'C', 'N']
+    ]
+
+    b = part2()
+    print(f'{b = }')
+
+    aocd.submit(answer=a, part='a', year=AOC_YEAR, day=AOC_DAY)
+    aocd.submit(answer=b, part='b', year=AOC_YEAR, day=AOC_DAY)
