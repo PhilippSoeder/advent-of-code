268606314a6f5817bc9da1fdb43388bfaa2cf934
Philipp Söder <philippsoeder@gmx.de>
2022-12-09 07:57:08 +0100

    Rename variables

--- day09/main.py
+++ day09/main.py
@@ -6,9 +6,9 @@ import math
 
 
 def run(n: int) -> int:
-    visited = {}
+    v = {}
     k = [(0, 0) for _ in range(n+1)]
-    visited[k[len(k)-1]] = True
+    v[k[len(k)-1]] = True
     for i in instructions:
         for _ in range(i[1]):
             match i[0]:
@@ -21,21 +21,21 @@ def run(n: int) -> int:
                 case 'L':
                     k[0] = (k[0][0]-1, k[0][1])
             for j in range(1, len(k)):
-                k[j] = move_tail(k[j-1], k[j])
-            visited[k[len(k)-1]] = True
-    return len(visited)
+                k[j] = move_knot(k[j-1], k[j])
+            v[k[len(k)-1]] = True
+    return len(v)
 
 
-def move_tail(H, T):
-    if abs(H[0]-T[0]) > 1 or abs(H[1]-T[1]) > 1:
-        d = (H[0]-T[0], H[1]-T[1])
+def move_knot(k1, k2):
+    if abs(k1[0]-k2[0]) > 1 or abs(k1[1]-k2[1]) > 1:
+        d = (k1[0]-k2[0], k1[1]-k2[1])
         if d[0] == 0:
-            T = (T[0], int(T[1]+sign(d[1])))
+            k2 = (k2[0], int(k2[1]+sign(d[1])))
         elif d[1] == 0:
-            T = (int(T[0]+sign(d[0])), T[1])
+            k2 = (int(k2[0]+sign(d[0])), k2[1])
         else:
-            T = (int(T[0]+sign(d[0])), int(T[1]+sign(d[1])))
-    return T
+            k2 = (int(k2[0]+sign(d[0])), int(k2[1]+sign(d[1])))
+    return k2
 
 
 def sign(x):
