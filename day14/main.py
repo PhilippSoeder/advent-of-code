4496b746d196b7b6a146e30fc70adc58cbedf34b
Philipp Söder <philippsoeder@gmx.de>
2022-12-14 13:50:15 +0100

    Optimise Day 14

--- day14/main.py
+++ day14/main.py
@@ -5,41 +5,7 @@ import aocd
 import sys
 
 
-def part1():
-    grid = []
-    for _ in range(0, max_y+1):
-        row = []
-        for _ in range(min_x, max_x+1):
-            row.append('.')
-        grid.append(row)
-
-    for p, path in enumerate(paths):
-        for i in range(len(path)-1):
-            x1 = paths[p][i][0] - min_x
-            x2 = paths[p][i+1][0] - min_x
-            if x1 > x2:
-                x1, x2 = x2, x1
-            y1 = paths[p][i][1]
-            y2 = paths[p][i+1][1]
-            if y1 > y2:
-                y1, y2 = y2, y1
-            if x1 == x2:
-                for i in range(y1, y2+1):
-                    grid[i][x1] = '#'
-            elif y1 == y2:
-                for i in range(x1, x2+1):
-                    grid[y1][i] = '#'
-
-    s = (500-min_x, 0)
-    grid[s[1]][s[0]] = '+'
-
-    a = 0
-    while sand_fall(grid, s, 0) is None:
-        a += 1
-    return a
-
-
-def part2():
+def create_grid(part2):
     grid = []
     extra_x = max_y
     for _ in range(0, max_y+1):
@@ -48,7 +14,9 @@ def part2():
             row.append('.')
         grid.append(row)
 
-    paths.append([(min_x-extra_x, max_y), (max_x+extra_x, max_y)])
+    if part2:
+        paths.append([(min_x-extra_x, max_y), (max_x+extra_x, max_y)])
+
     for p, path in enumerate(paths):
         for i in range(len(path)-1):
             x1 = paths[p][i][0] - min_x + extra_x
@@ -65,44 +33,27 @@ def part2():
             elif y1 == y2:
                 for i in range(x1, x2+1):
                     grid[y1][i] = '#'
-
     s = (500-min_x + extra_x, 0)
     grid[s[1]][s[0]] = '+'
-
-    a = 0
-    while sand_fall2(grid, s, 0, s) is None:
-        a += 1
-    return a
+    return grid, s
 
 
-def sand_fall(grid, p, a):
+def sand_flow(grid, p, part2):
     if p[1]+1 > max_y:
-        return a
+        if part2:
+            return
+        return 0
     if grid[p[1]+1][p[0]] != '#' and grid[p[1]+1][p[0]] != 'o':
-        return sand_fall(grid, (p[0], p[1]+1), a)
+        return sand_flow(grid, (p[0], p[1]+1), part2)
     else:
         if grid[p[1]+1][p[0]-1] != '#' and grid[p[1]+1][p[0]-1] != 'o':
-            return sand_fall(grid, (p[0]-1, p[1]+1), a)
+            return sand_flow(grid, (p[0]-1, p[1]+1), part2)
         elif grid[p[1]+1][p[0]+1] != '#' and grid[p[1]+1][p[0]+1] != 'o':
-            return sand_fall(grid, (p[0]+1, p[1]+1), a)
+            return sand_flow(grid, (p[0]+1, p[1]+1), part2)
         else:
             grid[p[1]][p[0]] = 'o'
-
-
-def sand_fall2(grid, p, a, s):
-    if p[1]+1 > max_y:
-        return None
-    if grid[p[1]+1][p[0]] != '#' and grid[p[1]+1][p[0]] != 'o':
-        return sand_fall2(grid, (p[0], p[1]+1), a, s)
-    else:
-        if grid[p[1]+1][p[0]-1] != '#' and grid[p[1]+1][p[0]-1] != 'o':
-            return sand_fall2(grid, (p[0]-1, p[1]+1), a, s)
-        elif grid[p[1]+1][p[0]+1] != '#' and grid[p[1]+1][p[0]+1] != 'o':
-            return sand_fall2(grid, (p[0]+1, p[1]+1), a, s)
-        else:
-            grid[p[1]][p[0]] = 'o'
-            if p == s:
-                return a
+            if part2 and p == s:
+                return 0
 
 
 if __name__ == '__main__':
@@ -135,12 +86,19 @@ if __name__ == '__main__':
             if max_y < point[1]:
                 max_y = point[1]
 
-    a = part1()
+    # Part 1
+    grid, s = create_grid(part2=False)
+    a = 0
+    while sand_flow(grid, s, part2=False) != 0:
+        a += 1
     print(f'{a = }')
 
+    # Part 2
     max_y += 2
-
-    b = part2() + 1
+    grid, s = create_grid(part2=True)
+    b = 1
+    while sand_flow(grid, s, part2=True) != 0:
+        b += 1
     print(f'{b = }')
 
     aocd.submit(answer=a, part='a', year=AOC_YEAR, day=AOC_DAY)
